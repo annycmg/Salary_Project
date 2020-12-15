40cb9141664ce7ab33934b844d84c9b98f8aff28
ANNY GONCALVES <anny_cmgoncalves@hotmail.com>
2020-12-15 20:05:25 -0300

    Input file

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import glassdoor_scraper as gs
 import pandas as pd
 path = "C:/Users/anny_/Documents/DataScience_Project/chromedriver_win32/chromedriver.exe"
 df = gs.get_jobs("data scientist", 15, False, path, 15)
-df.to_excel("C:\\Users\\anny_\\Documents\\DataScience_Project\\input\\input.xlsx")
+df.to_excel("C:\\Users\\anny_\\Documents\\DataScience_Project\\input_glassdoor.xlsx", index=False)
