e6292cca53e1437e6084fa91a96d47e9ec60b900
ANNY GONCALVES <anny_cmgoncalves@hotmail.com>
2020-12-16 00:39:11 -0300

    Some data cleaning

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,3 +1,33 @@
 import pandas as pd
 
-    
+df = pd.read_csv('C:\\Users\\anny_\\Documents\\The_Glassdoor_Project\\input_glassdoor.csv')    
+
+df['Per Hour'] = df['Salary Estimate'].apply(lambda x: 1 if 'per hour' in x.lower() else 0)
+df['Employer_Provided'] = df['Salary Estimate'].apply(lambda x: 1 if 'employer provided' in x.lower() else 0)
+
+df = df[df['Salary Estimate']!='-1']
+salary = df['Salary Estimate'].apply(lambda x: x.split('(')[0])
+minus_Kd = salary.apply(lambda x: x.replace('K','').replace('$',''))
+min_hr = minus_Kd.apply(lambda x: x.lower().replace('per hour', '').replace('employer provided', ''))
+
+df['min_salary'] = min_hr.apply(lambda x: int(x.split('-')[0])) 
+df['max_salary'] = min_hr.apply(lambda x: int(x.split('-')[1]))
+df['med_salary'] = (df.min_salary+df.max_salary)/2
+
+df['Company Name'] = df.apply(lambda x: x['Company Name'] if x['Rating'] < 0 else x['Company Name'][:-3], axis=1)
+
+df['State'] = df['Location'].apply(lambda x: x.split(',')[1])
+
+df['age'] = df['Founded'].apply(lambda x: x if x < 1 else 2020 - x)
+
+df['python_yn'] = df['Job Description'].apply(lambda x: 1 if 'python' in x.lower() else 0)
+
+df['R_yn'] = df['Job Description'].apply(lambda x: 1 if 'r studio' in x.lower() or 'r-studio' in x.lower() else 0)
+
+df['spark'] = df['Job Description'].apply(lambda x: 1 if 'spark' in x.lower() else 0)
+
+df['aws'] = df['Job Description'].apply(lambda x: 1 if 'aws' in x.lower() else 0)
+
+df['excel'] = df['Job Description'].apply(lambda x: 1 if 'excel' in x.lower() else 0)
+
+df.to_excel("C:\\Users\\anny_\\Documents\\The_Glassdoor_Project\\input_glassdoor.xlsx")
