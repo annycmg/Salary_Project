30228a6cb00390496c7bc94b67e487cdb5c647e1
ANNY GONCALVES <anny_cmgoncalves@hotmail.com>
2021-02-04 23:04:30 -0300

    start eda after data cleaning

---
+++ Packages/data_cleaning.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+def data_cleaning(caminho):
+    df = pd.read_csv(caminho + '\\Output\\glassdoor_jobs.csv')    
+
+    df['Per Hour'] = df['Salary Estimate'].apply(lambda x: 1 if 'per hour' in x.lower() else 0)
+    df['Employer_Provided'] = df['Salary Estimate'].apply(lambda x: 1 if 'employer provided' in x.lower() else 0)
+
+    df = df[df['Salary Estimate']!='-1']
+    salary = df['Salary Estimate'].apply(lambda x: x.split('(')[0])
+
+    minus_Kd = salary.apply(lambda x: x.replace('K','').replace('$',''))
+    min_hr = minus_Kd.apply(lambda x: x.lower().replace('per hour', '').replace('employer provided salary:', ''))
+
+    df['min_salary'] = min_hr.apply(lambda x: int(x.split('-')[0])) 
+    df['max_salary'] = min_hr.apply(lambda x: int(x.split('-')[1]))
+    df['med_salary'] = (df['min_salary'] + df['max_salary'])/2
+
+    df['Company Name'] = df.apply(lambda x: x['Company Name'] if x['Rating'] < 0 else x['Company Name'][:-3], axis=1)
+
+    df['State'] = df['Location'].apply(lambda x: x.split(',')[1])
+
+    df['age'] = df['Founded'].apply(lambda x: x if x < 1 else 2020 - x)
+
+    df['python_yn'] = df['Job Description'].apply(lambda x: 1 if 'python' in x.lower() else 0)
+
+    df['R_yn'] = df['Job Description'].apply(lambda x: 1 if 'r studio' in x.lower() or 'r-studio' in x.lower() else 0)
+
+    df['spark'] = df['Job Description'].apply(lambda x: 1 if 'spark' in x.lower() else 0)
+
+    df['aws'] = df['Job Description'].apply(lambda x: 1 if 'aws' in x.lower() else 0)
+
+    df['excel'] = df['Job Description'].apply(lambda x: 1 if 'excel' in x.lower() else 0)
+
+    df.drop(columns=['Unnamed: 0'], inplace=True)
+
+    df.to_excel(caminho + "\\Output\\glassdoor_jobs.xlsx", index=False)
---
+++ Packages/web_scraper.py
@@ -0,0 +1,169 @@
+"""
+# Web scraping algorithm that extracts job offers data
+# author: Kenarapfaik
+url: https://github.com/arapfaik/scraping-glassdoor-selenium
+"""
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
+from selenium import webdriver
+import time
+import pandas as pd
+
+def get_jobs(keyword, num_jobs, verbose, path, slp_time):
+    
+    '''Gathers jobs as a dataframe, scraped from Glassdoor'''
+    
+    options = webdriver.ChromeOptions() # Webdriver
+    
+    # Chromedriver path
+    driver = webdriver.Chrome(executable_path=path, options=options)
+    driver.set_window_size(1120, 1000)
+    
+    # Page URL 
+    url = "https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=false&clickSource=searchBtn&typedKeyword="+keyword+"&sc.keyword="+keyword+"&locT=&locId=&jobType="
+    driver.get(url)
+    jobs = []
+
+    while len(jobs) < num_jobs:  # If true, it keeps looking for new jobs.
+
+        time.sleep(slp_time) # Let the page load
+
+        # Get rid of SipnUp pop up screen.
+        try:
+            driver.find_element_by_class_name("selected").click()
+        except ElementClickInterceptedException:
+            pass
+        time.sleep(.1)
+        try:
+            driver.find_element_by_css_selector('[alt="Close"]').click() # Clicks [X] button.
+            print(' x out worked')
+        except NoSuchElementException:
+            print(' x out failed')
+            pass
+        
+        # Going through each job offer in this page
+        job_buttons = driver.find_elements_by_class_name("jl")
+        for job_button in job_buttons:  
+
+            print("Progress: {}".format("" + str(len(jobs)) + "/" + str(num_jobs))) # Print the amount of job offers clicked.
+            if len(jobs) >= num_jobs:
+                break
+            job_button.click() 
+            time.sleep(1)
+            collected_successfully = False
+            
+            while not collected_successfully: # Infos scraped from each job offer visited
+                try:
+                    company_name = driver.find_element_by_xpath('.//div[@class="employerName"]').text
+                    location = driver.find_element_by_xpath('.//div[@class="location"]').text
+                    job_title = driver.find_element_by_xpath('.//div[contains(@class, "title")]').text
+                    job_description = driver.find_element_by_xpath('.//div[@class="jobDescriptionContent desc"]').text
+                    collected_successfully = True
+                except:
+                    time.sleep(5)
+            try:
+                salary_estimate = driver.find_element_by_xpath('.//span[@class="gray salary"]').text
+            except NoSuchElementException:
+                salary_estimate = -1 # Value for not founded salary."
+            try:
+                rating = driver.find_element_by_xpath('.//span[@class="rating"]').text
+            except NoSuchElementException:
+                rating = -1 # Value for not founded rating."
+
+            #Printing for debugging
+            if verbose:
+                print("Job Title: {}".format(job_title))
+                print("Salary Estimate: {}".format(salary_estimate))
+                print("Job Description: {}".format(job_description[:500]))
+                print("Rating: {}".format(rating))
+                print("Company Name: {}".format(company_name))
+                print("Location: {}".format(location))
+
+            try: # Go to Company tab.
+                driver.find_element_by_xpath('.//div[@class="tab" and @data-tab-type="overview"]').click()
+
+                try:
+                    headquarters = driver.find_element_by_xpath('.//div[@class="infoEntity"]//label[text()="Headquarters"]//following-sibling::*').text
+                except NoSuchElementException:
+                    headquarters = -1
+
+                try:
+                    size = driver.find_element_by_xpath('.//div[@class="infoEntity"]//label[text()="Size"]//following-sibling::*').text
+                except NoSuchElementException:
+                    size = -1
+
+                try:
+                    founded = driver.find_element_by_xpath('.//div[@class="infoEntity"]//label[text()="Founded"]//following-sibling::*').text
+                except NoSuchElementException:
+                    founded = -1
+
+                try:
+                    type_of_ownership = driver.find_element_by_xpath('.//div[@class="infoEntity"]//label[text()="Type"]//following-sibling::*').text
+                except NoSuchElementException:
+                    type_of_ownership = -1
+
+                try:
+                    industry = driver.find_element_by_xpath('.//div[@class="infoEntity"]//label[text()="Industry"]//following-sibling::*').text
+                except NoSuchElementException:
+                    industry = -1
+
+                try:
+                    sector = driver.find_element_by_xpath('.//div[@class="infoEntity"]//label[text()="Sector"]//following-sibling::*').text
+                except NoSuchElementException:
+                    sector = -1
+
+                try:
+                    revenue = driver.find_element_by_xpath('.//div[@class="infoEntity"]//label[text()="Revenue"]//following-sibling::*').text
+                except NoSuchElementException:
+                    revenue = -1
+
+                try:
+                    competitors = driver.find_element_by_xpath('.//div[@class="infoEntity"]//label[text()="Competitors"]//following-sibling::*').text
+                except NoSuchElementException:
+                    competitors = -1
+
+            except NoSuchElementException:  # Rarely, some job offers do not have the "Company" tab, then -1 is the result.
+                headquarters = -1
+                size = -1
+                founded = -1
+                type_of_ownership = -1
+                industry = -1
+                sector = -1
+                revenue = -1
+                competitors = -1
+
+            if verbose:
+                print("Headquarters: {}".format(headquarters))
+                print("Size: {}".format(size))
+                print("Founded: {}".format(founded))
+                print("Type of Ownership: {}".format(type_of_ownership))
+                print("Industry: {}".format(industry))
+                print("Sector: {}".format(sector))
+                print("Revenue: {}".format(revenue))
+                print("Competitors: {}".format(competitors))
+                print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+
+            jobs.append({"Job Title" : job_title,
+            "Salary Estimate" : salary_estimate,
+            "Job Description" : job_description,
+            "Rating" : rating,
+            "Company Name" : company_name,
+            "Location" : location,
+            "Headquarters" : headquarters,
+            "Size" : size,
+            "Founded" : founded,
+            "Type of ownership" : type_of_ownership,
+            "Industry" : industry,
+            "Sector" : sector,
+            "Revenue" : revenue,
+            "Competitors" : competitors})            
+            
+        # Clicking on the [next page] button until it stores all the offers parametered.
+        try:
+            driver.find_element_by_xpath('.//li[@class="next"]//a').click()
+        except NoSuchElementException:
+            print("Scraping terminated before reaching target number of jobs. Needed {}, got {}.".format(num_jobs, len(jobs)))
+            break
+    df = pd.DataFrame(jobs)  # Convert dict object into DataFrame.
+    df.to_csv('C:\\Users\\anny_\\Documents\\The_Glassdoor_Project\\Ouput\\glassdoor_jobs.csv', index = False)
+    
+get_jobs('data scientist', 1000, False, "C:\\Users\\anny_\\Documents\\The_Glassdoor_Project\\chromedriver_win32", 15)
